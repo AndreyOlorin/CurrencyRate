301b776fbeef40c4f95d889104af9bc5c3fb14ae
AndreyOlorin <sap7andy@gmail.com>
2025-06-12 21:11:43 +0700

    CurrencyDomain2

--- app/src/main/java/com/example/tt_exchangerate/domain/CurrencyDomain.kt
+++ app/src/main/java/com/example/tt_exchangerate/domain/CurrencyDomain.kt
@@ -2,5 +2,7 @@ package com.example.tt_exchangerate.domain
 
 data class CurrencyDomain(
     val abbreviationName: String,
-    val decryptionName: String
+    val decryptionName: String,
+    val courseNow: Double,
+    val courseLast: Double
 )
