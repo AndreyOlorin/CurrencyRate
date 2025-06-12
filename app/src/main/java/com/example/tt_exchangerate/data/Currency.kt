56f803e1e79e8a266e91fcee7308fd4eb8d0cdec
AndreyOlorin <sap7andy@gmail.com>
2025-06-12 20:45:17 +0700

    Currency2

--- app/src/main/java/com/example/tt_exchangerate/data/Currency.kt
+++ app/src/main/java/com/example/tt_exchangerate/data/Currency.kt
@@ -3,4 +3,6 @@ package com.example.tt_exchangerate.data
 data class Currency(
     val abbreviationName: String,
     val decryptionName: String,
+    val courseNow: Double,
+    val courseLast: Double
 )
