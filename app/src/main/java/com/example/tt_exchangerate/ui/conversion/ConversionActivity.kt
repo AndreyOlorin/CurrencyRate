e5d8108b8758baa923339fa39042814d569e2105
AndreyOlorin <sap7andy@gmail.com>
2025-07-15 21:15:30 +0700

    ConversionActivity

---
+++ app/src/main/java/com/example/tt_exchangerate/ui/conversion/ConversionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tt_exchangerate.ui.conversion
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tt_exchangerate.databinding.ActivityConversionBinding
+
+class ConversionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConversionBinding
+
+}
